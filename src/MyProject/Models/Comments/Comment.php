6efb7c4b6c12f5a42aa6bd2290cfb59912efa4ed
WrSr12 <12nick12nick@gmail.com>
2023-11-24 09:24:51 +0300

    Add Comment properties

--- src/MyProject/Models/Comments/Comment.php
+++ src/MyProject/Models/Comments/Comment.php
@@ -6,6 +6,25 @@ use MyProject\Models\ActiveRecordEntity;
 
 class Comment extends ActiveRecordEntity
 {
+    /**
+     * @var int
+     */
+    protected $authorId;
+
+    /**
+     * @var int
+     */
+    protected $articleId;
+
+    /**
+     * @var string
+     */
+    protected $text;
+
+    /**
+     * @var string
+     */
+    protected $createdAt;
 
     protected static function getTableName(): string
     {
